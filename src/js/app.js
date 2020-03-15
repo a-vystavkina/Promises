2fa00aa2841effe3b2518071b9f825d7f3669205
Alexandra Vystavkina <a.vystavkina@yandex.ru>
2020-03-15 18:11:53 +0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+import GameSavingLoader from './GameSavingLoader';
+
+GameSavingLoader.load().then((saving) => {
+  // saving объект класса GameSaving
+  console.log(saving);
+}, (error) => {
+  // ...
+  console.log(error);
+});
